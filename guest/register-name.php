513eaf98e2a70e4a7ed69b6675bc87fd94cf6816
izzett57 <rustic_imports0j@icloud.com>
2024-06-23 19:17:01 +0800

    chore: moved import bootstrap script to a separate file

--- guest/register-name.php
+++ guest/register-name.php
@@ -5,14 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <!-- Import Bootstrap start -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <?php 
+            include('../assets/import-bootstrap.php');
+        ?>
         <!-- Import Bootstrap end -->
 
-        <!-- Import CSS file start -->
+        <!-- Import CSS file(s) start -->
         <link rel="stylesheet" href="../assets/css/guest.css">
-        <!-- Import CSS file end -->      
+        <!-- Import CSS file(s) end -->      
 
         <title>BookItClassroom</title>
     </head>
