f65fcce9e157dfebd1ee31abd438fa7aee1283b4
teacuptrash <100132413+teacuptrash@users.noreply.github.com>
2024-06-28 00:33:01 +0800

    chore: Modify `users` table auto-increment value to 1 made a new bookitclassroom with the usertype was set to enum which sets it to only member, admin, and club leader

--- sql/bookitclassroom-new-update.sql
+++ sql/bookitclassroom-new-update.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 23, 2024 at 07:59 AM
+-- Generation Time: Jun 27, 2024 at 06:31 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -34,7 +34,7 @@ CREATE TABLE `users` (
   `email` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `User_Type` varchar(200) NOT NULL
+  `User_Type` enum('Member','Admin','Club Leader') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
