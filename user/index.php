e997353bbae25bb019a47b8faaf5c86a7cb0b8bd
teacuptrash <100132413+teacuptrash@users.noreply.github.com>
2024-07-04 22:47:53 +0800

    chore: Update logout link in user dropdown menu

--- user/index.php
+++ user/index.php
@@ -40,7 +40,7 @@
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end p-2">
                       <li><a class="dropdown-item inter-regular" href="#">Profile</a></li>
-                      <li><a class="dropdown-item inter-regular" href="#">Sign Out</a></li>
+                      <li><a class="dropdown-item inter-regular" href="logout.php">Sign Out</a></li>
                     </ul>
                 </li>
                 <!-- Profile button end -->
