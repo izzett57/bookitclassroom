308446c1bfa53fa8bfa96cfc0da78cf02775bdfd
teacuptrash <100132413+teacuptrash@users.noreply.github.com>
2024-07-12 17:40:43 +0800

    chore: updated new column for user for profil picture

--- sql/bookitclassroom.sql
+++ sql/bookitclassroom.sql
@@ -1,51 +1,160 @@
--- Initialize Database
-CREATE DATABASE bookitclassroom;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2024 at 11:39 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
--- Use Database
-USE bookitclassroom;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Create USER Table
-CREATE TABLE USER (
-    ID int(20) NOT NULL AUTO_INCREMENT,
-    FName varchar(200) NOT NULL,
-    LName varchar(200) NOT NULL,
-    Email varchar(200) NOT NULL UNIQUE,
-    Password varchar(200) NOT NULL,
-    Date timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    User_Type enum('MEMBER','LECTURER','CLUB_LEAD','ADMIN') NOT NULL,
-    Reset_Token varchar(200),
-    Token_Expire DATETIME,
-    PRIMARY KEY(ID)
-);
 
--- Create CLASSROOM Table
-CREATE TABLE CLASSROOM (
-    CName varchar(200) NOT NULL UNIQUE,
-    Floor int(1) NOT NULL,
-    PRIMARY KEY(CName)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Create ENTRY Table
-CREATE TABLE ENTRY (
-    ID int(20) NOT NULL AUTO_INCREMENT,
-    User_ID int(20) NOT NULL,
-    EName varchar(200) NOT NULL,
-    Day enum('MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY') NOT NULL,
-    Time_Start TIME NOT NULL,
-    Time_End TIME NOT NULL,
-    Assigned_Class varchar(200),
-    FOREIGN KEY (User_ID) REFERENCES USER(ID),
-    FOREIGN KEY (Assigned_Class) REFERENCES CLASSROOM(CName),
-    PRIMARY KEY(ID)
-);
+--
+-- Database: `bookitclassroom`
+--
 
--- Create BOOKING Table
-CREATE TABLE BOOKING (
-    ID int(20) NOT NULL AUTO_INCREMENT,
-    Booking_Date DATE NOT NULL,
-    Entry_ID int(20) NOT NULL,
-    Classroom varchar(200) NOT NULL,
-    FOREIGN KEY (Entry_ID) REFERENCES ENTRY(ID),
-    FOREIGN KEY (Classroom) REFERENCES CLASSROOM(CName),
-    PRIMARY KEY(ID)
-);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `ID` int(20) NOT NULL,
+  `Booking_Date` date NOT NULL,
+  `Entry_ID` int(20) NOT NULL,
+  `Classroom` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classroom`
+--
+
+CREATE TABLE `classroom` (
+  `CName` varchar(200) NOT NULL,
+  `Floor` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `entry`
+--
+
+CREATE TABLE `entry` (
+  `ID` int(20) NOT NULL,
+  `User_ID` int(20) NOT NULL,
+  `EName` varchar(200) NOT NULL,
+  `Day` enum('MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY') NOT NULL,
+  `Time_Start` time NOT NULL,
+  `Time_End` time NOT NULL,
+  `Assigned_Class` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `ID` int(20) NOT NULL,
+  `FName` varchar(200) NOT NULL,
+  `LName` varchar(200) NOT NULL,
+  `Email` varchar(200) NOT NULL,
+  `Password` varchar(200) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `User_Type` enum('MEMBER','LECTURER','CLUB_LEAD','ADMIN') NOT NULL,
+  `Reset_Token` varchar(200) DEFAULT NULL,
+  `Token_Expire` datetime DEFAULT NULL,
+  `ProfilePicture` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `Entry_ID` (`Entry_ID`),
+  ADD KEY `Classroom` (`Classroom`);
+
+--
+-- Indexes for table `classroom`
+--
+ALTER TABLE `classroom`
+  ADD PRIMARY KEY (`CName`),
+  ADD UNIQUE KEY `CName` (`CName`);
+
+--
+-- Indexes for table `entry`
+--
+ALTER TABLE `entry`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `User_ID` (`User_ID`),
+  ADD KEY `Assigned_Class` (`Assigned_Class`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `ID` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `entry`
+--
+ALTER TABLE `entry`
+  MODIFY `ID` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `ID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`Entry_ID`) REFERENCES `entry` (`ID`),
+  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`Classroom`) REFERENCES `classroom` (`CName`);
+
+--
+-- Constraints for table `entry`
+--
+ALTER TABLE `entry`
+  ADD CONSTRAINT `entry_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `user` (`ID`),
+  ADD CONSTRAINT `entry_ibfk_2` FOREIGN KEY (`Assigned_Class`) REFERENCES `classroom` (`CName`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
