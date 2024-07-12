f9564d0cb337e22ab6a9bcdb857376387971a7af
teacuptrash <100132413+teacuptrash@users.noreply.github.com>
2024-07-12 20:51:53 +0800

    pull file from front end

--- guest/reset-password.php
+++ guest/reset-password.php
@@ -1,61 +1,116 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- Import Bootstrap start -->
+        <?php 
+            include('../assets/import-bootstrap.php');
+        ?>
+        <!-- Import Bootstrap end -->
 
-$token = $_POST["token"];
+        <!-- Import CSS file(s) start -->
+        <link rel="stylesheet" href="../assets/css/global.css">
+        <link rel="stylesheet" href="../assets/css/font-sizing.css">
+        <link rel="stylesheet" href="../assets/css/google-fonts.css">
+        <!-- Import CSS file(s) end -->
 
-$token_hash = hash("sha256", $token);
+        <title>Reset Password - BookItClassroom</title>
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
+    </head>
+    <body>
+        <!-- Nav bar start -->
+        <nav class="navbar bg-transparent px-5 py-4">
+            <div class="container-fluid">
+                <div class="d-flex align-items-center">
+                    <!-- Back button start -->
+                    <button onclick="history.back()" type="button" class="btn btn-light btn-circle me-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor" class="bi bi-arrow-left-circle-fill primary" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+                            </svg>
+                    </button>
+                    <!-- Back button end -->
+                    <!-- Logo start -->
+                    <a class="navbar-brand" href="index.php">
+                        <img src="../assets/logo.png" class="img-fluid" width="316" height="51">
+                    </a>
+                    <!-- Logo end -->
+                </div>
+                <!-- Profile button start -->
+                <li class="dropdown" style="list-style-type: none;">
+                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button type="button" class="btn btn-light btn-circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor" class="bi bi-person-circle primary" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                              </svg>
+                        </button>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end p-2">
+                        <li><a class="dropdown-item inter-regular" href="register-name.php">Register</a></li>
+                        <li><a class="dropdown-item inter-regular" href="login.php">Sign In</a></li>
+                    </ul>
+                </li>
+                <!-- Profile button end -->
+            </div>
+        </nav>
+        <!-- Nav bar end -->
 
-$mysqli = require __DIR__ . "/db_conn.php";
-
-$sql = "SELECT * FROM user
-        WHERE Reset_Token = ?";
-
-$stmt = $mysqli->prepare($sql);
-
-$stmt->bind_param("s", $token_hash);
-
-$stmt->execute();
-
-$result = $stmt->get_result();
-
-$user = $result->fetch_assoc();
-
-if ($user === null) {
-    die("token not found");
-}
-
-if (strtotime($user["Token_Expire"]) <= time()) {
-    die("token has expired");
-}
-
-if (strlen($_POST["password"]) < 8) {
-    die("Password must be at least 8 characters");
-}
-
-if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
-    die("Password must contain at least one letter");
-}
-
-if ( ! preg_match("/[0-9]/", $_POST["password"])) {
-    die("Password must contain at least one number");
-}
-
-if ($_POST["password"] !== $_POST["password_confirmation"]) {
-    die("Passwords must match");
-}
-
-$password_hash = Password($_POST["password"], PASSWORD_DEFAULT);
-
-$sql = "UPDATE user
-        SET Password = ?,
-            Reset_Token = NULL,
-            Token_Expire = NULL
-        WHERE id = ?";
-
-$stmt = $mysqli->prepare($sql);
-
-$stmt->bind_param("ss", $password_hash, $user["id"]);
-
-$stmt->execute();
-
-header("Location: forgot-password-complete.php");
-?>
+        <!-- Main content start -->
+        <div class="container main-content bg-white rounded-3 d-flex flex-column justify-content-center">
+            <div class="row justify-content-evenly">
+                    <!-- Text start -->
+                    <div class="row mb-5 text-center">
+                        <!-- Heading -->
+                        <span class="heading1" style="padding-bottom: 15px;">Reset password</span>
+                        <!-- Subheading -->
+                        <span class="subheading1" style="">Please enter a new password.</span>
+                    </div>
+                    <!-- Text end -->
+                <div class="row-auto d-flex flex-column">
+                    <div class="container" style="width: 50%;">
+                        <!-- Forgot password form start -->
+                        <form class="row">
+                            <!-- Email input start -->
+                            <div class="col">
+                                <label class="form-label inter-regular" for="Password" style="letter-spacing: 4px; color: #272937; text-transform: uppercase;">Password</label><br>
+                                <input class="form-control" id="Password" name="Password" type="Password" placeholder="password"><br>
+                            </div>
+                            <div class="col">
+                                <label class="form-label inter-regular" for="confirmPassword" style="letter-spacing: 4px; color: #272937; text-transform: uppercase;">Confirm Password</label><br>
+                                <input class="form-control" id="confirmPassword" name="confirmPassword" type="password" placeholder="confirm password"><br>
+                            </div>
+                            <!-- Email input end -->
+                            <div class="row pt-4">
+                                <!-- Spacing start -->
+                                <div class="col">
+                                </div>
+                                <!-- Spacing end -->
+                                <!-- Buttons start -->
+                                <div class="col d-flex justify-content-end align-items-center">
+                                    <!-- Back button start -->
+                                    <a onclick="href='index.php'" class="dongle-regular custom-btn-inline me-3 mt-2 primary" style="text-decoration: none; font-size: 2rem; cursor: pointer;">cancel</a>
+                                    <!-- Back button end -->
+                                    <!-- Next button start -->
+                                    <button type="submit" name="#" value="#" class="btn btn-lg custom-btn-noanim d-flex align-items-center justify-content-between">
+                                        <p class="dongle-regular mt-2" style="font-size: 3rem; flex-grow: 1;">Reset</p>
+                                    </button>
+                                    <!-- Next button end -->
+                                </div>
+                                <!-- Buttons end -->
+                            </div>
+                        </form>
+                        <!-- Forgot password form end --> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Main content end -->
+        <!-- Footer -->
+        <?php 
+            include('../assets/footer.php');
+        ?>
+        <!-- Footer end -->
+    </body>
+</html>
