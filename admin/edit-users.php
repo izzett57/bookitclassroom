034df952b5cf1217d8894ff746ecc76a3fe702ed
teacuptrash <100132413+teacuptrash@users.noreply.github.com>
2024-07-12 18:56:29 +0800

    chore: Update user profile page to display user information and profile picture (for admin)

--- admin/edit-users.php
+++ admin/edit-users.php
@@ -1,3 +1,36 @@
+<?php
+session_start();
+include '../assets/db_conn.php';
+
+if (!isset($_SESSION['ID']) || $_SESSION['User_Type'] !== 'ADMIN') {
+    header("Location: ../guest/login.php");
+    exit();
+}
+
+// Handle user deletion
+if (isset($_POST['delete_user'])) {
+    $delete_id = $_POST['delete_user'];
+    $delete_query = "DELETE FROM USER WHERE ID = ?";
+    $delete_stmt = $conn->prepare($delete_query);
+    $delete_stmt->bind_param("i", $delete_id);
+    if ($delete_stmt->execute()) {
+        $_SESSION['success_message'] = "User deleted successfully.";
+    } else {
+        $_SESSION['error_message'] = "Error deleting user.";
+    }
+    $delete_stmt->close();
+    header("Location: edit-users.php");
+    exit();
+}
+
+// Fetch all users
+$query = "SELECT ID, FName, LName, Email, User_Type FROM USER ORDER BY ID";
+$result = $conn->query($query);
+$users = $result->fetch_all(MYSQLI_ASSOC);
+
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -29,7 +62,7 @@
                     <button onclick="history.back()" type="button" class="btn btn-light btn-circle me-3">
                         <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor" class="bi bi-arrow-left-circle-fill primary" viewBox="0 0 16 16">
                             <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
-                            </svg>
+                        </svg>
                     </button>
                     <!-- Back button end -->
                     <!-- Logo start -->
@@ -45,7 +78,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor" class="bi bi-person-circle primary" viewBox="0 0 16 16">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                              </svg>
+                            </svg>
                         </button>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end p-2">
@@ -62,103 +95,52 @@
         <div class="container main-content bg-white rounded-3 d-flex flex-column justify-content-center">
             <div class="container">
                 <div class="row">
-                    <!-- Text start -->
                     <div class="col">
-                        <!-- Heading -->
                         <div class="heading1 ms-5"><p>Edit Users</p></div>
                         <div class="subheading1 ms-5"><p>Here is a list of users on BookItClassroom.</p></div>
                     </div>
-                    <!-- Text end -->
                 </div>
-                <!-- Profile start -->
+                <?php if (isset($_SESSION['success_message'])): ?>
+                    <div class="alert alert-success"><?php echo $_SESSION['success_message']; unset($_SESSION['success_message']); ?></div>
+                <?php endif; ?>
+                <?php if (isset($_SESSION['error_message'])): ?>
+                    <div class="alert alert-danger"><?php echo $_SESSION['error_message']; unset($_SESSION['error_message']); ?></div>
+                <?php endif; ?>
                 <div class="row">
-                <table class="table table-striped table-hover text-center inter-regular">
-                    <thead>
-                        <tr>
-                        <th scope="col" style="width: 3%;">#</th>
-                        <th scope="col" style="width: 31%;">User</th>
-                        <th scope="col" style="width: 21%;">Occupation</th>
-                        <th scope="col" style="width: 31%;">Email</th>
-                        <th scope="col" style="width: 14%;">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <!-- Placeholder start -->
-                        <tr>
-                        <th scope="row">1</th>
-                        <td>John Smith</td>
-                        <td>Lecturer</td>
-                        <td>johnsmith@email.com</td>
-                        <!-- Action start -->
-                        <td class="d-flex justify-content-evenly">
-                        <a class="custom-btn-inline" href="edit-entry.php" style="text-decoration: none;">
-                            Edit
-                            <i class="bi bi-pencil-fill"></i>    
-                        </a>
-                        <a class="custom-btn-inline" href="reserve-event.php" style="text-decoration: none;">
-                            Delete
-                            <i class="bi bi-trash-fill"></i>
-                        </a>
-                        <!-- Action end -->
-                        </svg>
-                        </td>
-                        </tr>
-                        <tr>
-                        <th scope="row">2</th>
-                        <td>Lara Jean</td>
-                        <td>Lecturer</td>
-                        <td>larajean@email.com</td>
-                        <!-- Action start -->
-                        <td class="d-flex justify-content-evenly">
-                        <a class="custom-btn-inline" href="edit-entry.php" style="text-decoration: none;">
-                            Edit
-                            <i class="bi bi-pencil-fill"></i>    
-                        </a>
-                        <a class="custom-btn-inline" href="reserve-event.php" style="text-decoration: none;">
-                            Delete
-                            <i class="bi bi-trash-fill"></i>
-                        </a>
-                        <!-- Action end -->
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                        <td>Bo Xiang</td>
-                        <td>Student Club Leader</td>
-                        <td>boxiang@email.com</td>
-                        <!-- Action start -->
-                        <td class="d-flex justify-content-evenly">
-                        <a class="custom-btn-inline" href="edit-entry.php" style="text-decoration: none;">
-                            Edit
-                            <i class="bi bi-pencil-fill"></i>    
-                        </a>
-                        <a class="custom-btn-inline" href="reserve-event.php" style="text-decoration: none;">
-                            Delete
-                            <i class="bi bi-trash-fill"></i>
-                        </a>
-                        <!-- Action end -->
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                        <td>Izzett Izam</td>
-                        <td>Member</td>
-                        <td>izzettizam@email.com</td>
-                        <!-- Action start -->
-                        <td class="d-flex justify-content-evenly">
-                        <a class="custom-btn-inline" href="edit-entry.php" style="text-decoration: none;">
-                            Edit
-                            <i class="bi bi-pencil-fill"></i>    
-                        </a>
-                        <a class="custom-btn-inline" href="reserve-event.php" style="text-decoration: none;">
-                            Delete
-                            <i class="bi bi-trash-fill"></i>
-                        </a>
-                        <!-- Action end -->
-                        </tr>
-                        <!-- Placeholder end -->
-                    </tbody>
-                </table>
+                    <table class="table table-striped table-hover text-center inter-regular">
+                        <thead>
+                            <tr>
+                                <th scope="col" style="width: 3%;">#</th>
+                                <th scope="col" style="width: 31%;">User</th>
+                                <th scope="col" style="width: 21%;">Occupation</th>
+                                <th scope="col" style="width: 31%;">Email</th>
+                                <th scope="col" style="width: 14%;">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($users as $index => $user): ?>
+                            <tr>
+                                <th scope="row"><?php echo $index + 1; ?></th>
+                                <td><?php echo htmlspecialchars($user['FName'] . ' ' . $user['LName']); ?></td>
+                                <td><?php echo htmlspecialchars($user['User_Type']); ?></td>
+                                <td><?php echo htmlspecialchars($user['Email']); ?></td>
+                                <td class="d-flex justify-content-evenly">
+                                    <a class="custom-btn-inline" href="edit-userprofile.php?id=<?php echo $user['ID']; ?>" style="text-decoration: none;">
+                                        Edit
+                                        <i class="bi bi-pencil-fill"></i>    
+                                    </a>
+                                    <form method="POST" onsubmit="return confirm('Are you sure you want to delete this user?');">
+                                        <button type="submit" name="delete_user" value="<?php echo $user['ID']; ?>" class="custom-btn-inline" style="background: none; border: none; color: inherit; cursor: pointer; text-decoration: none;">
+                                            Delete
+                                            <i class="bi bi-trash-fill"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
                 </div>
-                <!-- Profile end -->
             </div>
         </div>
         <!-- Main content end -->
