7930c112c9cf5f9c30491223894da96eb4c18de0
izzett57 <rustic_imports0j@icloud.com>
2024-07-13 14:31:48 +0800

    chore: changed titles

--- user/edit-entry-confirm.php
+++ user/edit-entry-confirm.php
@@ -18,7 +18,7 @@
 
         <link rel="stylesheet" href="../assets/css/entry.css">
 
-        <title>Edit Entry - Time - BookItClassroom</title>
+        <title>Edit Entry - Confirmation - BookItClassroom</title>
         <link rel="icon" type="image/x-icon" href="favicon.ico">
     
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
