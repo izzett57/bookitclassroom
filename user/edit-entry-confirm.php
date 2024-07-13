e588834614fca45f7e43d2cdd5b5d6a387996d4f
izzett57 <rustic_imports0j@icloud.com>
2024-07-13 14:30:41 +0800

    feat: added new & edit entry pages

---
+++ user/edit-entry-confirm.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- Import Bootstrap start -->
+        <?php 
+            include('../assets/import-bootstrap.php');
+        ?>
+        <!-- Import Bootstrap end -->
+
+        <!-- Import CSS file(s) start -->
+        <link rel="stylesheet" href="../assets/css/global.css">
+        <link rel="stylesheet" href="../assets/css/font-sizing.css">
+        <link rel="stylesheet" href="../assets/css/google-fonts.css">
+        <!-- Import CSS file(s) end --> 
+
+        <link rel="stylesheet" href="../assets/css/entry.css">
+
+        <title>Edit Entry - Time - BookItClassroom</title>
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
+    
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    
+    </head>
+    
+    <body>
+        <!-- Nav bar start -->
+        <nav class="navbar bg-transparent px-5 py-4">
+            <div class="container-fluid">
+                <div class="d-flex align-items-center">
+                    <!-- Back button start -->
+                    <button onclick="history.back()" type="button" class="btn btn-light btn-circle me-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor" class="bi bi-arrow-left-circle-fill primary" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+                            </svg>
+                    </button>
+                    <!-- Back button end -->
+                    <!-- Logo start -->
+                    <a class="navbar-brand" href="index.php">
+                        <img src="../assets/logo.png" class="img-fluid" width="316" height="51">
+                    </a>
+                    <!-- Logo end -->
+                </div>
+                <!-- Profile button start -->
+                <li class="dropdown" style="list-style-type: none;">
+                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button type="button" class="btn btn-light btn-circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor" class="bi bi-person-circle primary" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                              </svg>
+                        </button>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end p-2">
+                        <li><a class="dropdown-item inter-regular" href="profile.php">Profile</a></li>
+                        <li><a class="dropdown-item inter-regular" href="logout.php">Sign Out</a></li>
+                    </ul>
+                </li>
+                <!-- Profile button end -->
+            </div>
+        </nav>
+        <!-- Nav bar end -->
+        <!-- Main content start -->
+        <div class="container main-content bg-white rounded-3 d-flex flex-column justify-content-center">
+            <div class="container">
+                <div class="row">
+                    <!-- Text start -->
+                    <div class="col-9">
+                        <!-- Heading -->
+                        <div class="heading1 ms-5"><p>Edit Entry</p></div>
+                        <div class="subheading1 ms-5"><p>Are the information for the class/event correct?</p></div>
+                    </div>
+                    <!-- Text end -->
+                </div>
+                <div class="row-auto d-flex flex-column">
+                    <div class="container" style="height: 45vh; width: 90%; align-content: center;">
+                        <!-- Event day form start -->
+                        <form>
+                            <div class="d-flex justify-content-center align-items-center">
+                                <div class="dayBox">
+                                <span class="dayBoxText heading1">Wednesday</span>
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-center text-center mt-5">
+                                <span class="d-flex justify-content-center align-items-center timeBox text-time me-5">
+                                    00:00
+                                </span>
+                                <span class="text-time d-flex justify-content-center align-items-center ">
+                                    -
+                                </span>
+                                <span class="d-flex justify-content-center align-items-center timeBox text-time ms-5">
+                                    01:00
+                                </span>
+                            </div>
+                            <!-- Buttons start -->
+                                <div class="col d-flex justify-content-end align-items-center mt-5">
+                                    <!-- Back button start -->
+                                    <a onclick="history.back()" class="dongle-regular custom-btn-inline me-3 mt-2 primary" style="text-decoration: none; font-size: 2rem; cursor: pointer;">back</a>
+                                    <!-- Back button end -->
+                                    <!-- Next button start -->
+                                    <button type="submit" class="btn btn-lg custom-btn-noanim d-flex align-items-center justify-content-between">
+                                        <p class="dongle-regular mt-2" style="font-size: 3rem; flex-grow: 1;">Next</p>
+                                    </button>
+                                    <!-- Next button end -->
+                                </div>
+                                <!-- Buttons end -->
+                        </form>
+                        <!-- Event day form end -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Main content end -->
+        <!-- Footer -->
+        <?php 
+            include('../assets/footer.php');
+        ?>
+        <!-- Footer end -->
+    </body>
+</html>
