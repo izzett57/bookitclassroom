876c08b53ef6c0aac2529f215be8f8041f40cded
izzett57 <rustic_imports0j@icloud.com>
2024-07-16 00:03:51 +0800

    fix: admin page

--- admin/index.php
+++ admin/index.php
@@ -21,37 +21,44 @@
     </head>
     <body>
         <!-- Nav bar start -->
-        <nav class="navbar navbar-expand-lg bg-transparent px-5 py-4">
-            <div class="container-fluid">
-                <!-- Logo start-->
-                <a class="navbar-brand" href="index.php">
-                    <img src="../assets/logo.png" class="img-fluid" width="316" height="51">
-                </a>
-                <!-- Logo end -->
-                <!-- Profile Button -->
-                <li class="dropdown" style="list-style-type: none;">
-                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <button type="button" class="btn btn-light btn-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor" class="bi bi-person-circle primary" viewBox="0 0 16 16">
-                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                              </svg>
-                        </button>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end p-2">
-                        <li><a class="dropdown-item inter-regular" href="profile.php">Profile</a></li>
-                        <li><a class="dropdown-item inter-regular" href="logout.php">Sign Out</a></li>
-                    </ul>
-                </li>
-                <!-- Profile button end -->
-            </div>
-        </nav>
-        <!-- Nav bar end -->
-
-        <!-- Main content start -->
         <?php 
             include('../assets/navbar-user.php');
         ?>
+        <!-- Nav bar end -->
+
+        <!-- Main content start -->
+        <div class="container main-content bg-white rounded-3 d-flex flex-column justify-content-center">
+            <div class="row justify-content-evenly">
+                <div class="col-7 d-flex justify-content-center align-items-center">
+                    <!-- Text start -->
+                    <div>
+                        <!-- Heading -->
+                        <div class="heading1"><p>Welcome, Admin!</p></div>
+                        <!-- Subheading -->
+                        <div class="subheading1"><p>What would you like to do today?</p></div>
+                    </div>
+                    <!-- Text end -->
+                </div>
+                <div class="col d-flex flex-column align-items-center justify-content-center">
+                    <!-- View map button start -->
+                    <button onclick="location.href='#'" type="button" class="btn custom-btn btn-lg d-flex align-items-center justify-content-between mb-3" style="border-radius: 36px;">
+                        <p class="dongle-regular mt-2" style="font-size: 3rem; flex-grow: 1;">View Map</p>
+                        <span class="bg-light d-flex rounded-5 align-items-center justify-content-center" style="font-size: 1.5rem;">
+                            <i class="bi bi-pin-map-fill primary"></i>
+                        </span>
+                    </button>
+                    <!-- View map button end -->
+                    <!-- Edit users button -->
+                    <button onclick="location.href='edit-users.php'" type="button" class="btn custom-btn btn-lg d-flex align-items-center justify-content-between" style="border-radius: 36px;">
+                        <p class="dongle-regular mt-2" style="font-size: 3rem; flex-grow: 1;">Edit Users</p>
+                        <span class="bg-light d-flex rounded-5 align-items-center justify-content-center" style="font-size: 1.5rem;">
+                            <i class="bi bi-pencil-square primary"></i>
+                        </span>
+                    </button>
+                    <!-- Edit users end -->
+                </div>
+            </div>
+        </div>
         <!-- Main content end -->
         <!-- Footer -->
         <?php 
