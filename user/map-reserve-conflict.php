ce483263ab40971c261653b742f429f2b8011bdb
izzett57 <rustic_imports0j@icloud.com>
2024-07-17 00:32:55 +0800

    feat: Add reserve type selection page for booking and reserve when entry has conflicting times

---
+++ user/map-reserve-conflict.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- Import Bootstrap start -->
+        <?php 
+            include('../assets/import-bootstrap.php');
+        ?>
+        <!-- Import Bootstrap end -->
+
+        <!-- Import CSS file(s) start -->
+        <link rel="stylesheet" href="../assets/css/global.css">
+        <link rel="stylesheet" href="../assets/css/font-sizing.css">
+        <link rel="stylesheet" href="../assets/css/google-fonts.css">
+        <link rel="stylesheet" href="../assets/css/entry.css"/>
+        <!-- Import CSS file(s) end -->
+
+        <title>Confirm Reserve Changes - Map - BookItClassroom</title>
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
+    </head>
+    <body>
+        <!-- Nav bar start -->
+        <?php 
+            include('../assets/navbar-user-back.php');
+        ?>
+        <!-- Nav bar end -->
+
+        <!-- Main content start -->
+        <div class="container main-content bg-white rounded-3 d-flex flex-column justify-content-center">
+            <div class="row justify-content-evenly">
+                    <!-- Text start -->
+                    <div class="row d-flex justify-content-center mb-5 text-center">
+                        <!-- Heading -->
+                        <span class="heading1" style="padding-bottom: 30px;">Confirm Reserve</span>
+                        <!-- Subheading -->
+                        <span class="subheading1" style="width: 70%;">This event has a different timing than the one selected on the map, continuing will update the selected event to the respective timing.</span>
+                    </div>
+                    <!-- Text end -->
+                <div class="row-auto d-flex flex-column">
+                    <div class="container" style="width: 35%;">
+                        <!-- Reserve conflict form start -->
+                        <form>
+                            <div class="d-flex flex-column justify-content-center align-items-center mb-4">
+                                <p class="inter-regular" style="letter-spacing: 4px; color: #272937;text-transform: uppercase;">Event Name</p>
+                                <p class="subheading1" style="margin: 0px 0px 0px -2px;">Event Name</p>
+                            </div>
+                            <!-- Time select start -->
+                            <div class="col d-flex justify-content-center align-items-center" style="height: 16.66%;">
+                                <div class="d-flex flex-glow justify-content-center align-items-center" style="width: 100%;">
+                                    <div class="col-5 form-group text-center" style="width: 35%; height: 60px;">
+                                    <span class="d-flex justify-content-center align-items-center timeBox text-time" style="width: 100%; user-select: none;">
+                                        <!-- <?php echo date('H:i', strtotime($entry['start_time'])); ?> -->
+                                        01:00 <!-- placeholder -->
+                                    </span>
+                                    </div>
+                                    <span class="col-1 text-center text-time mx-2" style="user-select: none;">-</span>
+                                    <div class="col-5 form-group text-center" style="width: 35%; height: 60px;" style="width: 100%">
+                                    <span class="d-flex justify-content-center align-items-center timeBox text-time" style="width: 100%; user-select: none;">
+                                        <!-- <?php echo date('H:i', strtotime($entry['end_time'])); ?> -->
+                                        02:00 <!-- placeholder -->
+                                    </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Time select end -->
+                                <!-- Buttons start -->
+                                <div class="col d-flex justify-content-center align-items-center mt-5">
+                                    <!-- Back button start -->
+                                    <a onclick="history.back()" class="dongle-regular custom-btn-inline px-3 me-3 mt-2 primary" style="text-decoration: none; font-size: 2rem; cursor: pointer;">back</a>
+                                    <!-- Back button end -->
+                                    <!-- Next button start -->
+                                    <button type="submit" class="btn btn-lg custom-btn-noanim d-flex align-items-center justify-content-between">
+                                        <p class="dongle-regular mt-2" style="font-size: 3rem; flex-grow: 1;">Reserve</p>
+                                    </button>
+                                    <!-- Next button end -->
+                                </div>
+                                <!-- Buttons end -->
+                        </form>
+                        <!-- Reserve conflict form end -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Main content end -->
+        <!-- Footer -->
+        <?php 
+            include('../assets/footer.php');
+        ?>
+        <!-- Footer end -->
+    </body>
+</html>
---
+++ user/map-reserve-type-select.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- Import Bootstrap start -->
+        <?php 
+            include('../assets/import-bootstrap.php');
+        ?>
+        <!-- Import Bootstrap end -->
+
+        <!-- Import CSS file(s) start -->
+        <link rel="stylesheet" href="../assets/css/global.css">
+        <link rel="stylesheet" href="../assets/css/font-sizing.css">
+        <link rel="stylesheet" href="../assets/css/google-fonts.css">
+        <link rel="stylesheet" href="../assets/css/entry.css"/>
+        <!-- Import CSS file(s) end -->
+
+        <title>Reserve - Type - BookItClassroom</title>
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
+    </head>
+    <body>
+        <!-- Nav bar start -->
+        <?php 
+            include('../assets/navbar-user-back.php');
+        ?>
+        <!-- Nav bar end -->
+
+        <!-- Main content start -->
+        <div class="container main-content bg-white rounded-3 d-flex flex-column justify-content-center">
+            <div class="row justify-content-evenly">
+                    <!-- Text start -->
+                    <div class="row d-flex justify-content-center mb-5 text-center">
+                        <!-- Heading -->
+                        <span class="heading1" style="padding-bottom: 30px;">Choose Reserve Type</span>
+                        <!-- Subheading -->
+                        <span class="subheading1" style="width: 70%;">Would you like to reserve for a single timeslot or the whole semester?</span>
+                    </div>
+                    <!-- Text end -->
+                <div class="row-auto d-flex flex-column">
+                    <div class="container">
+                        <!-- Reserve type form start -->
+                        <form class="row justify-content-center">
+                            <!-- Spacing start -->
+                            <div class="col"></div>
+                            <!-- Spacing end -->
+                            <!-- Buttons start -->
+                            <div class="col d-flex justify-content-center align-items-center">
+                            <button type="button" name="" class="btn custom-btn btn-lg d-flex align-items-center justify-content-between mb-3" style="width: 340px; border-radius: 36px;">
+                                <p class="dongle-regular mt-2" style="font-size: 3rem; flex-grow: 1;">Single Booking</p>
+                                <span class="bg-light d-flex rounded-5 align-items-center justify-content-center" style="font-size: 1.5rem;">
+                                <i class="bi bi-1-circle-fill primary"></i>
+                                </span>
+                            </button>
+                            </div>
+                            <!-- Buttons end -->
+                            <!-- Buttons start -->
+                            <div class="col d-flex justify-content-center align-items-center">
+                            <button type="button" name="" class="btn custom-btn btn-lg d-flex align-items-center justify-content-between mb-3" style="width: 340px; border-radius: 36px;">
+                                <p class="dongle-regular mt-2" style="font-size: 3rem; flex-grow: 1;">Whole Semester</p>
+                                <span class="bg-light d-flex rounded-5 align-items-center justify-content-center" style="font-size: 1.5rem;">
+                                <i class="bi bi-calendar-week primary"></i>
+                                </span>
+                            </button>
+                            </div>
+                            <!-- Buttons end -->
+                            <!-- Spacing start -->
+                            <div class="col"></div>
+                            <!-- Spacing end -->
+                        </form>
+                        <!-- Reserve type form end -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Main content end -->
+        <!-- Footer -->
+        <?php 
+            include('../assets/footer.php');
+        ?>
+        <!-- Footer end -->
+    </body>
+</html>
