b645a8808fff4f2d599647d9f4bbbbca66483840
izzett57 <rustic_imports0j@icloud.com>
2024-07-18 23:29:21 +0800

    feat: added select floor

--- user/select-floor.php
+++ user/select-floor.php
@@ -39,19 +39,46 @@ if (!isset($_SESSION['ID'])) {
                         <div class="subheading1 ms-5"><p>Which floor would you like to view?</p></div>
                     </div>
                 </div>
-                <div class="row-auto d-flex flex-column">
-                    <div class="container" style="height: 45vh; width: 90%; align-content: center;">
+                <div class="row-auto d-flex flex-column pb-5">
+                    <div class="container" style="width: 90%; align-content: center;">
                         <form method="POST">
                             <div class="d-flex flex-column justify-content-center align-items-center pt-1">
-                                <div>
-                                <object id="svg-object" type="image/svg+xml" data="../assets/svg/map/floor-1.svg" style="width: 200px; height: 200px; transform: skew(0deg, 0deg);"></object>
+                                <!-- Floor start -->
+                                <div class="row" style="margin-bottom: -90px;">
+                                    <div class="col">
+                                        <img id="svg-object" src="../assets/svg/map/floor-3.svg" style="width: 200px; height: 200px; transform: scale(2.0, 1.0) rotateX(60deg) rotateY(0deg) rotateZ(-45deg);"></img>
+                                    </div>
+                                    <div class="col d-flex justify-content-center align-items-center" style="margin-left: 200px;">
+                                        <button type="submit" name="reserve" value="yes" class="btn btn-lg custom-btn-noanim d-flex align-items-center" style="display: inline-block; position: absolute;">
+                                            <p class="dongle-regular mt-2" style="font-size: 3rem; flex-grow: 1;">3F</p>
+                                        </button>
+                                    </div>
                                 </div>
-                                <div>
-                                    test 2
+                                <!-- Floor end -->
+                                <!-- Floor start -->
+                                <div class="row" style="margin-bottom: -90px;">
+                                    <div class="col">
+                                        <img id="svg-object" src="../assets/svg/map/floor-2.svg" style="width: 200px; height: 200px; transform: scale(2.0, 1.0) rotateX(60deg) rotateY(0deg) rotateZ(-45deg);"></img>
+                                    </div>
+                                    <div class="col d-flex justify-content-center align-items-center" style="margin-left: 200px;">
+                                        <button type="submit" name="reserve" value="yes" class="btn btn-lg custom-btn-noanim d-flex align-items-center" style="display: inline-block; position: absolute">
+                                            <p class="dongle-regular mt-2" style="font-size: 3rem; flex-grow: 1;">2F</p>
+                                        </button>
+                                    </div>
                                 </div>
-                                <div>
-                                    test 3
+                                <!-- Floor end -->
+                                <!-- Floor start -->
+                                <div class="row" style="margin-bottom: -90px;">
+                                    <div class="col">
+                                        <img id="svg-object" src="../assets/svg/map/floor-1.svg" style="width: 200px; height: 200px; transform: scale(2.0, 1.0) rotateX(60deg) rotateY(0deg) rotateZ(-45deg);"></img>
+                                    </div>
+                                    <div class="col d-flex justify-content-center align-items-center" style="margin-left: 200px;">
+                                        <button type="submit" name="reserve" value="yes" class="btn btn-lg custom-btn-noanim d-flex align-items-center" style="display: inline-block; position: absolute">
+                                            <p class="dongle-regular mt-2" style="font-size: 3rem; flex-grow: 1;">1F</p>
+                                        </button>
+                                    </div>
                                 </div>
+                                <!-- Floor end -->
                             </div>
                         </form>
                     </div>
